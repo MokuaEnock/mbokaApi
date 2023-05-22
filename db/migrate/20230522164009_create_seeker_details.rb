b538c54ebab493740be61d2463bb990aed0d8e3c
Enock <enock.mokua@student.moringaschool.com>
2023-05-22 19:43:28 +0300

    create seeker details attributes

--- db/migrate/20230522164009_create_seeker_details.rb
+++ db/migrate/20230522164009_create_seeker_details.rb
@@ -2,6 +2,14 @@ class CreateSeekerDetails < ActiveRecord::Migration[7.0]
   def change
     create_table :seeker_details do |t|
       t.references :seeker, null: false, foreign_key: true
+      t.string :first_name
+      t.string :second_name
+      t.string :third_name
+      t.integer :phone_number
+      t.date :dob
+      t.string :gender
+      t.string :nationality
+      t.string :country
 
       t.timestamps
     end
